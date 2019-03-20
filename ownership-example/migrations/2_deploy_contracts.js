cd0da6df774db0f98b56b18840f9e232c8b32bb7
harishraisinghani <harishraisinghani@gmail.com>
2019-03-20 11:40:33 -0700

    Remove ownership example

--- ownership-example/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var Ownership = artifacts.require("./Ownership.sol");
-var OwnershipLink = artifacts.require("./OwnershipLink.sol");
-
-module.exports = function(deployer) {
-	deployer.deploy(Ownership);
-	deployer.deploy(OwnershipLink);
-};
